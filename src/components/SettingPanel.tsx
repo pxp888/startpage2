0c7ef06cbcd879e5d3802f1300b2d1324028b035
pxp888 <pxperrine@gmail.com>
2024-02-19 21:36:53 +0100

    ready for CSS

--- src/components/SettingPanel.tsx
+++ src/components/SettingPanel.tsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 import "./settingpanel.css";
 
 interface SettingPanelProps {
@@ -19,10 +21,32 @@ function SettingPanel({
 	imageChanged,
 	selected,
 }: SettingPanelProps) {
+	const [frameSize, setFrameSize] = useState(50);
+	const [cutSize, setCutSize] = useState(50);
+	const [backColor, setBackColor] = useState("#000000");
+	const [frameColor, setFrameColor] = useState("#000000");
+
+	function frameSizeChanged(size: number) {
+		setFrameSize(size);
+	}
+
+	function cutSizeChanged(size: number) {
+		setCutSize(size);
+	}
+
+	function backColorChanged(color: string) {
+		setBackColor(color);
+	}
+
+	function frameColorChanged(color: string) {
+		setFrameColor(color);
+	}
+
 	return (
 		<div>
 			<h1>Setting Panel</h1>
 			<div>
+				<label htmlFor="nameline">Name: </label>
 				<input
 					type="text"
 					id="nameline"
@@ -33,6 +57,7 @@ function SettingPanel({
 					}}
 					{...(selected === -1 ? {disabled: true} : {})}
 				/>
+				<label htmlFor="linkline">URL: </label>
 				<input
 					type="text"
 					id="linkline"
@@ -43,6 +68,7 @@ function SettingPanel({
 					}}
 					{...(selected === -1 ? {disabled: true} : {})}
 				/>
+				<label htmlFor="imageline">Image URL: </label>
 				<input
 					type="text"
 					id="imageline"
@@ -52,6 +78,48 @@ function SettingPanel({
 						imageChanged(e.target.value);
 					}}
 					{...(selected === -1 ? {disabled: true} : {})}
+				/>
+			</div>
+			<div id="pagesettings">
+				<label htmlFor="framesizecontrol">Frame Size </label>
+				<input
+					id="framesizecontrol"
+					type="range"
+					min="0"
+					max="100"
+					value={frameSize}
+					onChange={(e) => {
+						frameSizeChanged(parseInt(e.target.value));
+					}}
+				/>
+				<label htmlFor="cutsizecontrol">Icon Size </label>
+				<input
+					id="cutsizecontrol"
+					type="range"
+					min="0"
+					max="100"
+					value={cutSize}
+					onChange={(e) => {
+						cutSizeChanged(parseInt(e.target.value));
+					}}
+				/>
+				<label htmlFor="backcolorcontrol">Color </label>
+				<input
+					id="backcolorcontrol"
+					type="color"
+					value={backColor}
+					onChange={(e) => {
+						backColorChanged(e.target.value);
+					}}
+				/>
+				<label htmlFor="framecolorcontrol">Color </label>
+				<input
+					id="framecolorcontrol"
+					type="color"
+					value={frameColor}
+					onChange={(e) => {
+						frameColorChanged(e.target.value);
+					}}
 				/>
 			</div>
 		</div>
